7dfd02fc5c4d673c304063380947d2c9b120c525
José EA <jose@albatross.pe>
2019-01-05 07:06:43 -0500

    Fix #100: The last_ad_referral and is_payment_enabled fields are now deprecated.
    	Messenger.java new methods:
    
    	- queryUserProfile: original method only with basic fields.
    	- queryUserProfileFull: new method with full fields.

--- src/main/java/com/github/messenger4j/userprofile/UserProfile.java
+++ src/main/java/com/github/messenger4j/userprofile/UserProfile.java
@@ -1,6 +1,5 @@
 package com.github.messenger4j.userprofile;
 
-import com.github.messenger4j.webhook.event.common.Referral;
 import java.util.Optional;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
@@ -17,15 +16,12 @@ public final class UserProfile {
     private final String firstName;
     private final String lastName;
     private final String profilePicture;
-    private final String locale;
-    private final float timezoneOffset;
-    private final Gender gender;
-    private final boolean isPaymentEnabled;
-    private final Optional<Referral> lastAdReferral;
+    private final Optional<String> locale;
+    private final Optional<Float> timezoneOffset;
+    private final Optional<Gender> gender;
 
     public UserProfile(@NonNull String firstName, @NonNull String lastName, @NonNull String profilePicture,
-                       @NonNull String locale, float timezoneOffset, @NonNull Gender gender, boolean isPaymentEnabled,
-                       @NonNull Optional<Referral> lastAdReferral) {
+            @NonNull Optional<String> locale, Optional<Float> timezoneOffset, @NonNull Optional<Gender> gender) {
 
         this.firstName = firstName;
         this.lastName = lastName;
@@ -33,8 +29,6 @@ public final class UserProfile {
         this.locale = locale;
         this.timezoneOffset = timezoneOffset;
         this.gender = gender;
-        this.isPaymentEnabled = isPaymentEnabled;
-        this.lastAdReferral = lastAdReferral;
     }
 
     public String firstName() {
@@ -49,24 +43,16 @@ public final class UserProfile {
         return profilePicture;
     }
 
-    public String locale() {
+    public Optional<String> locale() {
         return locale;
     }
 
-    public float timezoneOffset() {
+    public Optional<Float> timezoneOffset() {
         return timezoneOffset;
     }
 
-    public Gender gender() {
+    public Optional<Gender> gender() {
         return gender;
-    }
-
-    public boolean isPaymentEnabled() {
-        return isPaymentEnabled;
-    }
-
-    public Optional<Referral> lastAdReferral() {
-        return lastAdReferral;
     }
 
     /**
