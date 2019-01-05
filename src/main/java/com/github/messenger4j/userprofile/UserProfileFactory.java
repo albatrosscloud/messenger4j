e01af0e8214b682be683a312132fad0f3fd7fcfc
José EA <jose@albatross.pe>
2019-01-05 08:13:46 -0500

    1.0.5

--- src/main/java/com/github/messenger4j/userprofile/UserProfileFactory.java
+++ src/main/java/com/github/messenger4j/userprofile/UserProfileFactory.java
@@ -8,7 +8,6 @@ import static com.github.messenger4j.internal.gson.GsonUtil.Constants.PROP_PROFI
 import static com.github.messenger4j.internal.gson.GsonUtil.Constants.PROP_TIMEZONE;
 import static com.github.messenger4j.internal.gson.GsonUtil.getPropertyAsFloat;
 import static com.github.messenger4j.internal.gson.GsonUtil.getPropertyAsString;
-import static java.util.Optional.empty;
 import com.google.gson.JsonObject;
 import java.util.Optional;
 
@@ -29,16 +28,13 @@ public final class UserProfileFactory {
         final String profilePic = getPropertyAsString(jsonObject, PROP_PROFILE_PIC)
                 .orElseThrow(IllegalArgumentException::new);
 
-        final Optional<String> locale = getPropertyAsString(jsonObject, PROP_LOCALE)
-                .orElse(empty());
+        final Optional<String> locale = getPropertyAsString(jsonObject, PROP_LOCALE);
 
-        final Optional<Float> timezoneOffset = getPropertyAsFloat(jsonObject, PROP_TIMEZONE)
-                .orElse(empty());
+        final Optional<Float> timezoneOffset = getPropertyAsFloat(jsonObject, PROP_TIMEZONE);
 
         final Optional<UserProfile.Gender> gender = getPropertyAsString(jsonObject, PROP_GENDER)
                 .map(String::toUpperCase)
-                .map(UserProfile.Gender::valueOf)
-                .orElse(empty());
+                .map(UserProfile.Gender::valueOf);
 
         return new UserProfile(firstName, lastName, profilePic, locale, timezoneOffset, gender);
     }
